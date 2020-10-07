452ffe9db4548c7152ddcb79d46121d5aedf29ed
Santiago Pavon <santiago@dit.upm.es>
2020-10-07 13:15:06 +0200

    Step 5.1: Authors of Quizzes.

--- models/index.js
+++ models/index.js
@@ -3,4 +3,9 @@ const Quiz = require('./quiz');
 const User = require('./user');
 const Session = require('./session');
 
+// Relation 1-to-N between User and Quiz:
+User.hasMany(Quiz, {as: 'quizzes', foreignKey: 'authorId'});
+Quiz.belongsTo(User, {as: 'author', foreignKey: 'authorId'});
+
+
 module.exports = sequelize;
